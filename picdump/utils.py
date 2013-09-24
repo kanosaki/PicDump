d5edb4c637e97dfb8c299611220895e0d8ac2de7
kanosaki <kanosaki@gmail.com>
2013-09-24 14:32:41 +0900

    Add PageIterator and URLBuilder to utils.py

--- picdump/utils.py
+++ picdump/utils.py
@@ -1,3 +1,8 @@
+
+import queue
+import urllib.parse
+
+
 # Utility functions
 def cached_property(f):
     def get(self):
@@ -25,3 +30,98 @@ def constant_fn(const):
     def inner(*args, **kw):
         return const
     return inner
+
+
+class PageIterator:
+    def __init__(self, source):
+        self.source = source
+        self.queue = queue.Queue()
+        self.is_source_empty = False
+
+    def __next__(self):
+        self.prefetch(1)
+        try:
+            return self.queue.get_nowait()
+        except queue.Empty:
+            raise StopIteration()
+
+    def __iter__(self):
+        return self
+
+    def fetch(self):
+        try:
+            next_page = next(self.source)
+            for item in next_page:
+                self.queue.put(item)
+        except StopIteration:
+            self.is_source_empty = True
+
+    def prefetch(self, buffer_to):
+        if self.buffered_size < buffer_to and not self.is_source_empty:
+            self.fetch()
+            self.prefetch(buffer_to)
+
+    @property
+    def page_size(self):
+        return 50
+
+    @property
+    def buffered_size(self):
+        return self.queue.qsize()  # TODO: check validity.
+                                   # qsize method basically returns approximate
+                                   # size
+
+
+def with_prefix(prefix, value):
+    if value.startswith(prefix):
+        return value
+    else:
+        return prefix + value
+
+
+class URLBuilder:
+    def __init__(self, scheme='http',
+                 host='',
+                 path='',
+                 params={},
+                 fragment=None):
+        self._path = self._fragment = ''
+        self.scheme = scheme
+        self.host = host
+        self.path = path
+        self.params = params
+        self.fragment = fragment
+
+    @property
+    def path(self):
+        return self._path
+
+    @path.setter
+    def path(self, value):
+        self._path = with_prefix('/', value)
+
+    @property
+    def fragment(self):
+        return self._fragment
+
+    @fragment.setter
+    def fragment(self, value):
+        self._fragment = with_prefix('#', value)
+
+    @property
+    def query(self):
+        def mk_pair(kv):
+            return '{}={}'.format(kv[0],
+                                  urllib.parse.quote(str(kv[1]), safe=''))
+        pairs = map(mk_pair, self.params.items())
+        return '?' + '&'.join(pairs)
+
+    def __str__(self):
+        return '{}://{}{}{}{}'.format(self.scheme,
+                                      self.host,
+                                      self.path,
+                                      self.query,
+                                      self.fragment)
+
+    def to_request(self):
+        pass
--- test/test_utils.py
+++ test/test_utils.py
@@ -1,26 +1,71 @@
-
-from nose.tools import assert_equals
+import unittest
+import itertools
 
 import picdump.utils as utl
 
 
-class TestUtilFunctions:
+class TestUtilFunctions(unittest.TestCase):
     def test_id_fn(self):
         values = [1, "foo", object(), [], {}]
         for v in values:
-            assert_equals(v, utl.id_fn(v))
+            self.assertEqual(v, utl.id_fn(v))
 
     def test_void_fn(self):
         values = [1, "foo", object(), [], {}]
         for v in values:
-            assert_equals(None, utl.void_fn(v))
+            self.assertEqual(None, utl.void_fn(v))
 
-        assert_equals(None, utl.void_fn(None, None, None))
-        assert_equals(None, utl.void_fn(None, 1, 2, 3, hoge="fuga"))
+        self.assertEqual(None, utl.void_fn(None, None, None))
+        self.assertEqual(None, utl.void_fn(None, 1, 2, 3, hoge="fuga"))
 
     def test_constant_fn(self):
         const = "foo"
         const_fn = utl.constant_fn(const)
         values = [1, "foo", object(), [], {}]
         for v in values:
-            assert_equals(const, const_fn(v))
+            self.assertEqual(const, const_fn(v))
+
+
+class TestPageIterator(unittest.TestCase):
+    def test_empty(self):
+        source = iter([])
+        pi = utl.PageIterator(source)
+        self.assertEqual(list(pi), [])
+
+    def test_full_page_fetching(self):
+        pages = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        pi = utl.PageIterator(iter(pages))
+        self.assertEqual(list(pi), [elem for page in pages for elem in page])
+        self.assertTrue(pi.is_source_empty)
+
+    def test_partial_page_fetching(self):
+        pages = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        pi = utl.PageIterator(iter(pages))
+        taken = list(itertools.islice(pi, 4))
+        self.assertFalse(pi.is_source_empty)
+        self.assertEqual(taken, [1, 2, 3, 4])
+        self.assertEqual(pi.buffered_size, 2)
+        pi.prefetch(2)
+        self.assertEqual(pi.buffered_size, 2)
+        pi.prefetch(3)
+        self.assertEqual(pi.buffered_size, 5)
+        self.assertEqual(list(pi), [5, 6, 7, 8, 9])
+        self.assertTrue(pi.is_source_empty)
+
+
+class TestURLBuilder(unittest.TestCase):
+    def test_init(self):
+        builder = utl.URLBuilder(scheme="http",
+                                 host="www.example.com",
+                                 path="/some/path",
+                                 params={'foo': 1, 'bar': ' '},
+                                 fragment="frg")
+        expected = "http://www.example.com/some/path?bar=%20&foo=1#frg"
+        self.assertEqual(str(builder), expected)
+        builder.host = 'www.example2.com'
+        expected = "http://www.example2.com/some/path?bar=%20&foo=1#frg"
+        self.assertEqual(str(builder), expected)
+
+    def test_with_prefix(self):
+        self.assertEqual('/hoge/fuga', utl.with_prefix('/', 'hoge/fuga'))
+        self.assertEqual('/hoge/fuga', utl.with_prefix('/', '/hoge/fuga'))
