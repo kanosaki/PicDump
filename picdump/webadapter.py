708e56f5d0eda81d5103543364c827cceb5d5e01
kanosaki <kanosaki@gmail.com>
2013-09-26 14:53:18 +0900

    Implement webadapter

--- picdump/webadapter.py
+++ picdump/webadapter.py
@@ -1,3 +1,22 @@
 
+import urllib.request
+
+
 class WebAdapter:
-    pass
+    def get(self, urllike):
+        url = self.mk_url(urllike)
+        try:
+            res = urllib.request.urlopen(url)
+            return res.read()
+        except Exception as e:
+            raise e
+
+    def open(self, urllike):
+        url = self.mk_url(urllike)
+        try:
+            return urllib.request.urlopen(url)
+        except Exception as e:
+            raise e
+
+    def mk_url(self, urllike):
+        return str(urllike)
