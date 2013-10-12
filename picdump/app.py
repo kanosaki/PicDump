464bb8a36d203cb4a33bcc5bd110fc34fc46f239
kanosaki <kanosaki@gmail.com>
2013-10-12 23:14:53 +0900

    Add main routine

--- picdump/app.py
+++ picdump/app.py
@@ -5,3 +5,11 @@ class App:
     def __init__(self, config):
         self.config = config
 
+    def start(self):
+        for folder in self.config.folders:
+            folder.start_dump()
+
+
+class Dumper:
+    def __init__(self, config):
+        self.config = config
--- picdump/folder.py
+++ picdump/folder.py
@@ -1,3 +1,10 @@
+import datetime
+import itertools
+from picdump import scheduler
+
+
+DEFAULT_UPDATE_INTERVAL = datetime.timedelta(hours=1)
+
 
 class Folder:
     def __init__(self, path=None, source=None, updater=None):
@@ -10,3 +17,44 @@ class Folder:
         self.path = path
         self.source = source
         self.updater = updater
+
+    def start_dump(self):
+        raise NotImplementedError()
+
+    def clear(self):
+        raise NotImplementedError()
+
+    def sink(self, it):
+        raise NotImplementedError()
+
+
+class Updater(scheduler.Worker):
+    def __init__(self,
+                 interval=DEFAULT_UPDATE_INTERVAL,
+                 clear_dir=True,
+                 source_reset=True,
+                 size=50):
+        super().__init__(interval)
+        self.folder_clear = clear_dir
+        self.source_reset = source_reset
+        self.size = size
+        self.target_folder = None
+
+    def start(self, folder):
+        self.target_folder = folder
+        super().start()
+
+    def work(self):
+        folder = self.target_folder
+        if self.folder_clear:
+            folder.clear()
+        if self.source_reset:
+            folder.source.reset()
+        contents = itertools.islice(folder.source, self.size)
+        folder.sink(contents)
+
+
+
+
+
+
